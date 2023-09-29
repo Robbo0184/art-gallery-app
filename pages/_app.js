fab92481458fec9c125c9dfe51a27e2681bc6490
Rob <barrrobert84@gmail.com>
2023-09-29 16:57:12 +0200

    Added navbar functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import GlobalStyle from "../styles";
-import {SWRConfig }from 'swr';
+import useSWR from "swr";
+import Layout from "@/components/Layout/Layout";
+
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
-
+const URL = 'https://example-apis.vercel.app/api/art'
 
 
 
 export default function App({ Component, pageProps }) {
+  const { data: pieces, error, isLoading } = useSWR(URL, fetcher)
+
+  if (!pieces) return;
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <>
       <GlobalStyle />
-      <SWRConfig value={{fetcher}}>
-      <Component {...pageProps} />
-      </SWRConfig>
+
+      <Component {...pageProps} pieces={pieces} />
+      <Layout />
     </>
   );
 }
---
+++ pages/art-pieces/index.js
@@ -0,0 +1,12 @@
+import ArtPieces from "@/components/ArtPieces/ArtPieces";
+import Layout from "@/components/Layout/Layout";
+
+
+export default function ArtPiecesPage ({pieces}) {
+    return (
+        <>
+        <ArtPieces pieces={pieces} />
+        
+        </>
+    )
+}
