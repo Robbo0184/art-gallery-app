efe415cee8ae6754fbec6af8c1803e74a3269d83
Rob <barrrobert84@gmail.com>
2023-10-03 14:50:11 +0200

    Comments form reverted to div, curently functional

--- components/ArtPiece/ArtPieceDetails.js
+++ components/ArtPiece/ArtPieceDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FavouriteButton from "../FavouriteButton/FavouriteButton";
 import CommentForm from "../CommentForm/CommentForm";
-
+import Comments from "../Comments/Comments";
 import styled from "styled-components";
 import Link from "next/link";
 const StyledFormDiv = styled.div`
@@ -11,7 +11,7 @@ const StyledFormDiv = styled.div`
   padding: 3rem;
 `;
 
-import Comments from "../Comments/Comments";
+
 
 
 export function ArtPieceDetails({
@@ -22,26 +22,28 @@ export function ArtPieceDetails({
   name,
   artist,
   year,
-  comments, 
+  comments,
   onSubmitComment,
   genre,
 }) {
   return (
 
-    <StyledFormDiv>
-      <img width={500} height={500} src={image} alt="artpiece" />
-      <span>{`${name} by : ${artist}`}</span>
-      <p>{`Year:${year}`}</p>
-      <p>{`Genre: ${genre}`}</p>
-      <CommentForm onSubmitComment={onSubmitComment} />
+    <div>
+      {year} {genre}
+      <img width={400} height={400} src={image} alt="artpiece" />
+      <span>{`Famous Painting ${name} by Famous Artist: ${artist}`}</span>
+      <div className="section--comments">
+        <h2>Comments</h2>
+        <Comments comments={comments} />
+      </div>
       <div className="section--comment">
-        <br></br>
-       <FavouriteButton
+        <CommentForm slug={slug} onSubmitComment={onSubmitComment} />
+        <FavouriteButton
           slug={slug}
           isFavourite={isFavourite}
           onToggleFavourite={onToggleFavourite}
         />
       </div>
-    </StyledFormDiv>
+    </div>
   );
 }
